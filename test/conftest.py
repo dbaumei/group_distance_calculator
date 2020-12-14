494fd2da24e0622f9cd044498ea3e0d402796d14
dbaumei <dominicbaumeister@gmail.com>
2020-12-15 00:50:39 +0100

    Add fixtures to generate test data

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,13 @@
+from gdc import MeetingLocation, Member
 import openrouteservice as ors
 from os import path
 import pytest
+from typing import List
 
 
 @pytest.fixture
 def ors_client() -> ors.Client:
-    """Implements an open route service client.
-    Needs to have a valid path to an API token text file.
+    """Implement an open route service client. Needs to have a valid path to an API token text file.
 
     Yields:
         An open route service client.
@@ -18,3 +19,31 @@ def ors_client() -> ors.Client:
         ors_api_key = key_file.readline().strip()
 
     yield ors.Client(ors_api_key)
+
+@pytest.fixture
+def meeting_location_list(ors_client) -> List:
+    """Generate test data.
+
+    Returns:
+        List of meeting locations.
+
+    """
+    test_locations = [r'''{"name": "", "city": "Bocholt", "postal_code": "46395", "street": "Anholter Postweg", "housenumber": "3"}''',
+    r'''{"name": "", "city": "Raesfeld", "postal_code": "", "street": "", "housenumber": ""}''',
+    r'''{"name": "", "city": "Suedlohn", "postal_code": "", "street": "", "housenumber": ""}''']
+
+    meeting_locations = [MeetingLocation(ors_client, line) for line in test_locations]
+    return meeting_locations
+
+@pytest.fixture
+def member_list(ors_client) -> List:
+    """Generate test data.
+
+    Returns:
+        List of members.
+
+    """
+    test_members = [r'''{"name": "Lukas", "city": "Raesfeld", "postal_code": "", "street": "", "housenumber": ""}''',
+    r'''{"name": "Vincent", "city": "Bocholt", "postal_code": "", "street": "", "housenumber": ""}''']
+    members = [Member(ors_client, line) for line in test_members]
+    return members
