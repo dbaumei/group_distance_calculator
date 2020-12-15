08510e185e65d35d0a3e032f47c7d2059d27c9b4
dbaumei <dominicbaumeister@gmail.com>
2020-12-15 01:14:07 +0100

    Some more tests

--- test/test_group_distance_calculator.py
+++ test/test_group_distance_calculator.py
@@ -13,22 +13,29 @@ def test_it_fetches_a_time_distance_matrix(ors_client, member_list, meeting_loca
     check.greater(len(matrix['distances']), 0)
 
 
-def test_it_calculates_total_times_in_min(ors_client, member_list, meeting_location_list):
-    subject = GroupDistanceCalculator(ors_client, member_list, meeting_location_list)
+def test_it_calculates_total_times_in_min(ors_client):
+    subject = GroupDistanceCalculator(ors_client, [], [])
 
     test_matrix = {'durations': [[300, 600, 900], [120, 180, 1080]]}
-    sums = [30.0, 23.0]
-
+    sums_minutes = [7.0, 13.0, 33.0]
     result = subject.totalTimes(test_matrix)
 
-    check.almost_equal(result, sums, rel=0.001)
+    check.almost_equal(result, sums_minutes, rel=0.001)
 
-def test_it_calculates_total_distances_in_km(ors_client, member_list, meeting_location_list):
+def test_it_calculates_total_distances_in_km(ors_client):
+    subject = GroupDistanceCalculator(ors_client, [], [])
+
+    test_matrix = {'distances': [[4600.0, 1300.0], [1000.0, 2000.0], [5000.0, 7000.0]]}
+    sums_km = [11.0, 10.0]
+    result = subject.totalDistances(test_matrix)
+
+    check.almost_equal(result, sums_km, rel=0.001)
+
+# This test is not really necessary. The function only collects the output of other functions.
+# Just making sure it does not crash.
+def test_it_gets_total_times_and_distances(ors_client, member_list, meeting_location_list):
     subject = GroupDistanceCalculator(ors_client, member_list, meeting_location_list)
 
-    pass
-
-def test_it_gets_total_time_and_distance(ors_client, member_list, meeting_location_list):
-    subject = GroupDistanceCalculator(ors_client, member_list, meeting_location_list)
-
-    pass
+    result = subject.getTotals()
+    check.is_not_none(result['times'])
+    check.is_not_none(result['distances'])
